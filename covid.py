78847d8253767be0007ec90358035f9802fa5c07
IGATUS <66783226+IGATUS@users.noreply.github.com>
2021-04-24 13:14:42 +0100

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,8 +1,4 @@
-import streamlit as st 
-import pandas as pd 
-import base64
-import matplotlib.pyplot as plt 
-import numpy as np 
+import streamlit as st  
 import requests 
 from bs4 import BeautifulSoup
 
